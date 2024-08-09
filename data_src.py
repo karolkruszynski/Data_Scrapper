48d6d2a33a503cb45d40b264a62dc09da0bb50b7
karol <karol.kruszynski0@gmail.com>
2024-08-09 10:49:36 +0200

    feat: For every single sub-cat get products urls to individual list

--- data_src.py
+++ data_src.py
@@ -25,3 +25,19 @@ fixed_hrefs = [room for room in hrefs if not '=' in room]
 
 # Create full links to sub-cat
 full_links = [urljoin(url, sub_cat_url) for sub_cat_url in fixed_hrefs]
+
+# For every single sub-cat get products urls to individual list
+for sub_cat_link in full_links:
+    # Send a request to fetch the HTML content
+    response = requests.get(sub_cat_link)
+    try:
+        response.raise_for_status()  # Check for request errors
+    except Exception as e:
+        print(f'Failed to request: {e}')
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    find_products_selector = soup.find_all('div', class_='product-image')
+    find_product_link = [product.find('a')['href'] for product in find_products_selector if product.find('a')]
+    print(find_product_link)
