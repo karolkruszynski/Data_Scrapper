d6eb10e5d63360d75ab72301617d988bdc304568
karol <karol.kruszynski0@gmail.com>
2024-08-09 10:28:23 +0200

    init + find li selector with rooms

---
+++ data_src.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import os
+
+# Base URL
+url = 'https://www.lexington.com/'
+
+# Send a request to fetch the HTML content
+response = requests.get(url)
+try:
+    response.raise_for_status()  # Check for request errors
+except Exception as e:
+    print(f'Failed to request: {e}')
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
+
+find_cat_by_room = [room for room in soup.find_all('li', class_='level_4')]
