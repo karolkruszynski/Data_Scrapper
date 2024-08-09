1f74dc6ad73846bb77301ba45452cde0d7fe97e3
karol <karol.kruszynski0@gmail.com>
2024-08-09 11:06:28 +0200

    feat: add code to funct and return data in dict

--- data_src.py
+++ data_src.py
@@ -3,36 +3,13 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import os
 
-# Base URL
-url = 'https://www.lexington.com/'
+def main():
 
-# Send a request to fetch the HTML content
-response = requests.get(url)
-try:
-    response.raise_for_status()  # Check for request errors
-except Exception as e:
-    print(f'Failed to request: {e}')
+    # Base URL
+    url = 'https://www.lexington.com/'
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Find Hrefs to category
-find_cat_by_room = [room for room in soup.find_all('li', class_='level_4')]
-
-# Hrefs to sub-category by room ( products inside it )
-hrefs = [room.find('a')['href'] for room in find_cat_by_room if room.find('a')]
-fixed_hrefs = [room for room in hrefs if not '=' in room]
-
-# Create full links to sub-cat
-full_links = [urljoin(url, sub_cat_url) for sub_cat_url in fixed_hrefs]
-
-# Dict to store multi-cat with their list of links to products
-products_dict = {}
-
-# For every single sub-cat get products urls to individual list
-for sub_cat_link in full_links:
     # Send a request to fetch the HTML content
-    response = requests.get(sub_cat_link)
+    response = requests.get(url)
     try:
         response.raise_for_status()  # Check for request errors
     except Exception as e:
@@ -41,12 +18,44 @@ for sub_cat_link in full_links:
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    find_products_selector = soup.find_all('div', class_='product-image')
-    find_product_link = [product.find('a')['href'] for product in find_products_selector if product.find('a')]
-    print(find_product_link)
+    # Find Hrefs to category
+    find_cat_by_room = [room for room in soup.find_all('li', class_='level_4')]
 
-    # Adding a list of links to the dictionary under the appropriate key
-    products_dict[sub_cat_link] = find_product_link
+    # Hrefs to sub-category by room ( products inside it )
+    hrefs = [room.find('a')['href'] for room in find_cat_by_room if room.find('a')]
+    fixed_hrefs = [room for room in hrefs if not '=' in room]
 
-    # List of links for a given sub_cat_link
-    print(f"Links for {sub_cat_link}: {find_product_link}")
+    # Create full links to sub-cat
+    full_links = [urljoin(url, sub_cat_url) for sub_cat_url in fixed_hrefs]
+
+    # Dict to store multi-cat with their list of links to products
+    products_dict = {}
+
+    # For every single sub-cat get products urls to individual list
+    for sub_cat_link in full_links:
+        # Send a request to fetch the HTML content
+        response = requests.get(sub_cat_link)
+        try:
+            response.raise_for_status()  # Check for request errors
+        except Exception as e:
+            print(f'Failed to request: {e}')
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        find_products_selector = soup.find_all('div', class_='product-image')
+        find_product_link = [product.find('a')['href'] for product in find_products_selector if product.find('a')]
+        print(find_product_link)
+
+        # Adding a list of links to the dictionary under the appropriate key
+        products_dict[sub_cat_link] = find_product_link
+
+        # List of links for a given sub_cat_link
+        print(f"Links for {sub_cat_link}: {find_product_link}")
+
+        return products_dict  # Return Data
+
+    # Run def
+    if __name__ == "__main__":
+        final_products_dict = main()
+        print(final_products_dict)
