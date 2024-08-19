006bd38fd73a5cfbceb28b8f14f011eae2b541a3
HP <a9buklewska4e@gmail.com>
2024-08-19 17:18:59 +0200

    testing extract sizes and dims output

--- testing_script.py
+++ testing_script.py
@@ -13,23 +13,24 @@ def parse_html(html_content):
     """Parses the HTML content using BeautifulSoup."""
     return BeautifulSoup(html_content, 'html.parser')
 
-def extra_dimensions(soup):
-    """Extracts additional dimensions information like Arm Depth etc."""
-    # Find all <span> tags, excluding those with class 'dimensions'
-    exact_smaller_elements = [div for div in soup.find_all('div', class_='smaller')]
-    # Znajdź wszystkie <strong> i <span> wewnątrz tych <div> - Flat List
-    strong_tags = [strong for div in exact_smaller_elements for strong in div.find_all('strong')]
-    span_tags = [span for div in exact_smaller_elements for span in div.find_all('span')]
-    full_data = {}
-    for key, value in zip(strong_tags, span_tags):
-        strip_key = key.get_text(strip=True)
-        full_data[strip_key] = value.get_text(strip=True)
-    return full_data
+def extract_sizes_and_dimensions(soup):
+    """Extracts sizes and dimensions from h3 tags with the class 'showPrint'."""
+    sizes = soup.find_all('h3', class_='showPrint')
+    filtered_sizes = [h3 for h3 in sizes if 'first' not in h3.get('class', [])]
+    size_info = []
+    for h3 in filtered_sizes:
+        size_text = h3.contents[0].strip()
+        strong_tags = h3.find_all('strong')
+        if len(strong_tags) >= 2:
+            SKU = strong_tags[0].get_text(strip=True).replace('Item: ', '')
+            DIMS = strong_tags[1].get_text(strip=True).replace('Dimensions: ', '')
+            size_info.append((size_text, SKU, DIMS))
+    return size_info
 
 def main():
-    html_content = fetch_html("https://www.lexington.com/aidan-upholstered-arm-chair")
+    html_content = fetch_html("https://www.lexington.com/arlington-platform-bed")
     soup = parse_html(html_content)
-    data  = extra_dimensions(soup)
+    data  = extract_sizes_and_dimensions(soup)
     print(data)
 
 if __name__ == "__main__":
