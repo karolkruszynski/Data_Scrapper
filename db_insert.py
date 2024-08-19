9376f37ea4ae22683613a9fd4d16e34b48e3ced2
HP <a9buklewska4e@gmail.com>
2024-08-19 17:21:05 +0200

    feat: read data from db

--- db_insert.py
+++ db_insert.py
@@ -6,31 +6,8 @@ conn = sqlite3.connect('lexington.db')
 # Create Cursor
 cursor = conn.cursor()
 
-# Create Table
-creat_table = '''
-    CREATE TABLE lexington 
-    (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    stock TEXT NOT NULL,
-    sku TEXT NOT NULL,
-    dims TEXT NOT NULL
-    )
-'''
-#cursor.execute(creat_table)
-
-# Adding Data
-insert_commend = '''
-    INSERT INTO lexington (id, name, stock, sku, dims)
-    VALUES (?, ?, ?, ?, ?)
-'''
-cursor.execute(insert_commend,('2','arbor','In Stock', '714-144C', '83W x 90D x 74H in.'))
-
-# Make changes
-#conn.commit()
-
 # Odczytywanie danych
-cursor.execute('SELECT * FROM lexington')
+cursor.execute('SELECT * FROM product_other_versions')
 rows = cursor.fetchall()
 
 for row in rows:
